5debde907dd877c882a70012eb6fc51483af7b68
Guilherme Paterlini <guilhermepaterlini@gmail.com>
2024-01-29 07:19:52 -0400

    first commit

---
+++ src/components/Comment.tsx
@@ -0,0 +1,61 @@
+import { ThumbsUp, Trash } from "@phosphor-icons/react";
+import { Avatar } from "./Avatar";
+
+import styles from "./Comment.module.css";
+import { useState } from "react";
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        hasBorder={false}
+        src="https://github.com/guipaterlini.png"
+        alt=""
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Diego Fernandes</strong>
+              <time
+                title="14 de dezembro às 11:20"
+                dateTime="2023-12-14 11:20:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp /> Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
